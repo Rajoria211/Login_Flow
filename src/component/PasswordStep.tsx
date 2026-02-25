f0b820179fd28f4ecef3cb86da9cda2d7e840df2
Rajoria211 <priyanshurajoria2003@gmail.com>
2026-02-25 20:54:30 +0530

    Added the CSS for Password Step

--- src/component/PasswordStep.tsx
+++ src/component/PasswordStep.tsx
@@ -7,10 +7,19 @@ interface Props {
   updateData: (data: Partial<FormData>) => void;
 }
 
+interface ShowState {
+  password: boolean;
+  confirm: boolean;
+}
+
 export default function PasswordStep({ onNext, onBack, updateData }: Props) {
   const [password, setPassword] = useState("");
   const [confirm, setConfirm] = useState("");
   const [error, setError] = useState("");
+  const [show,setShow] = useState<ShowState>({
+    password:false,
+    confirm:false,
+  });
 
   const handleContinue = () => {
     if (password.length < 6) {
@@ -29,32 +38,40 @@ export default function PasswordStep({ onNext, onBack, updateData }: Props) {
 
   return (
     <div className="step-container">
-      <h2 className="step-title">Create Password for your account</h2>
+      <h2 className="password-step-title">Create Password for your account</h2>
 
-      <div className="form-group">
+      <div className="form-group password">
         <label>Enter new password</label>
         <input
-          type="password"
-          className="input"
+          type={show.password? "text":"password"}
+          className="input password"
           value={password}
           onChange={(e) => {
             setPassword(e.target.value);
             setError("");
           }}
         />
+        <button className="show-password"
+        onClick={()=>{setShow((prev)=>({...prev,password:!prev.password}))}}
+        ><img src="src/styles/images/eye_icon.png" alt="show" /></button>
+        <p className="password-guide">Must be atleast 6 characters</p>
       </div>
 
-      <div className="form-group">
+      <div className="form-group password">
         <label>Confirm password</label>
         <input
-          type="password"
-          className="input"
+          type={show.confirm? "text":"password"}
+          className="input password"
           value={confirm}
           onChange={(e) => {
             setConfirm(e.target.value);
             setError("");
           }}
-        />
+          />
+        <button className="show-password"
+        onClick={()=>{setShow((prev)=>({...prev,confirm:!prev.confirm}))}}
+        ><img src="src/styles/images/eye_icon.png" alt="show" /></button>
+        <p className="password-guide">Both passwords must match</p>
         {error && <p className="error-text">{error}</p>}
       </div>
 
